8888bf841f2bb765cddb5ff8929d9270c768f530
LoicNOEL-Shodo <loic.noel@shodo-reunion.io>
2025-08-25 09:42:34 +0400

    Add backend

---
+++ apps/backend/src/main/java/com/typologies/api/GettingStartedService.java
@@ -0,0 +1,11 @@
+package com.typologies.api;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GettingStartedService {
+
+    public String hello(String name) {
+        return String.format("Hello '%s'.", name);
+    }
+}
